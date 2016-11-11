668415ee3d9fc74390dae3f522c557682727ef29
malzma <markus.malz@it2media.de>
2016-11-11 22:03:13 +0100

    prepare for testing

---
+++ test/IT2media.Extensions.Logging.Abstractions.Test/LoggerExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IT2media.Extensions.Logging.Abstractions.Test
+{
+    public class LoggerExtensionsTest
+    {
+        [Fact]
+        public void Test1() 
+        {
+            
+        }
+
+        [Theory]
+        [InlineData("hahaha")]
+        [InlineData("")]
+        public void Test2()
+        {
+        }
+    }
+}
