7dff3311a205d1614e5140a8bdb59fc2d53a8ed0
malzma <markus.malz@it2media.de>
2016-11-22 08:07:08 +0100

    Centralized GetHashCode functions for reusing, implemented them for every logLevel

--- src/IT2media.Extensions.Logging.Abstractions/LoggerExtensions.cs
+++ src/IT2media.Extensions.Logging.Abstractions/LoggerExtensions.cs
@@ -23,26 +23,15 @@ namespace IT2media.Extensions.Logging.Abstractions
         public static void LogDebug(this ILogger logger, Exception exception, string message, params object[] args)
         {
             int hashCode = 0;
-
-            if (message == null)
-            {
-                if (exception != null && exception.Message != null)
-                {
-                    hashCode = exception.Message.GetHashCodeUShort();
-                }
-            }
-            else
-            {
-                hashCode = message.GetHashCodeUShort();
-            }
+            hashCode = GetHashCode(ref message, exception, hashCode);
 
             if (args == null)
             {
-                logger.LogDebug(hashCode, exception, string.Empty);
+                logger.LogDebug(hashCode, exception, message);
             }
             else
             {
-                logger.LogDebug(hashCode, exception, string.Empty, args);
+                logger.LogDebug(hashCode, exception, message, args);
             }
         }
 
@@ -55,10 +44,7 @@ namespace IT2media.Extensions.Logging.Abstractions
         public static void LogDebug(this ILogger logger, Exception exception, params object[] args)
         {
             int hashCode = 0;
-            if (exception != null && exception.Message != null) //currently this is not the case, but with different .NET Frameworks and Mono-Implementations we can't be sure, so let's do this safe
-            {
-                hashCode = exception.Message.GetHashCodeUShort();
-            }
+            hashCode = GetHashCodeFromException(exception, hashCode);
 
             if (args == null)
             {
@@ -81,13 +67,16 @@ namespace IT2media.Extensions.Logging.Abstractions
         /// <param name="args">An object array that contains zero or more objects to format.</param>
         public static void LogTrace(this ILogger logger, Exception exception, string message, params object[] args)
         {
-            if (message == null)
+            int hashCode = 0;
+            hashCode = GetHashCode(ref message, exception, hashCode);
+
+            if (args == null)
             {
-                logger.LogTrace(0, exception, string.Empty, args);
+                logger.LogTrace(hashCode, exception, message);
             }
             else
             {
-                logger.LogTrace(message.GetHashCodeUShort(), exception, message, args);
+                logger.LogTrace(hashCode, exception, message, args);
             }
         }
 
@@ -95,19 +84,22 @@ namespace IT2media.Extensions.Logging.Abstractions
         /// Formats and writes a trace log message.
         /// </summary>
         /// <param name="logger">The <see cref="ILogger"/> to write to.</param>
-        /// <param name="message">Format string of the log message.</param>
+        /// <param name="exception">The exception to log.</param>
         /// <param name="args">An object array that contains zero or more objects to format.</param>
-        //public static void LogTrace(this ILogger logger, string message, params object[] args)
-        //{
-        //    if (message == null)
-        //    {
-        //        logger.LogTrace(0, string.Empty, args);
-        //    }
-        //    else
-        //    {
-        //        logger.LogTrace(message.GetHashCodeUShort(), message, args);
-        //    }
-        //}
+        public static void LogTrace(this ILogger logger, Exception exception, params object[] args)
+        {
+            int hashCode = 0;
+            hashCode = GetHashCodeFromException(exception, hashCode);
+
+            if (args == null)
+            {
+                logger.LogTrace(hashCode, exception, string.Empty);
+            }
+            else
+            {
+                logger.LogTrace(hashCode, exception, string.Empty, args);
+            }
+        }
 
         //------------------------------------------INFORMATION------------------------------------------//
 
@@ -120,33 +112,39 @@ namespace IT2media.Extensions.Logging.Abstractions
         /// <param name="args">An object array that contains zero or more objects to format.</param>
         public static void LogInformation(this ILogger logger, Exception exception, string message, params object[] args)
         {
-            if (message == null)
+            int hashCode = 0;
+            hashCode = GetHashCode(ref message, exception, hashCode);
+
+            if (args == null)
             {
-                logger.LogInformation(0, exception, string.Empty, args);
+                logger.LogInformation(hashCode, exception, message);
             }
             else
             {
-                logger.LogInformation(message.GetHashCodeUShort(), exception, message, args);
+                logger.LogInformation(hashCode, exception, message, args);
             }
         }
 
         /// <summary>
-        /// Formats and writes an informational log message.
+        /// Formats and writes a information log message.
         /// </summary>
         /// <param name="logger">The <see cref="ILogger"/> to write to.</param>
-        /// <param name="message">Format string of the log message.</param>
+        /// <param name="exception">The exception to log.</param>
         /// <param name="args">An object array that contains zero or more objects to format.</param>
-        //public static void LogInformation(this ILogger logger, string message, params object[] args)
-        //{
-        //    if (message == null)
-        //    {
-        //        logger.LogInformation(0, string.Empty, args);
-        //    }
-        //    else
-        //    {
-        //        logger.LogInformation(message.GetHashCodeUShort(), message, args);
-        //    }
-        //}
+        public static void LogInformation(this ILogger logger, Exception exception, params object[] args)
+        {
+            int hashCode = 0;
+            hashCode = GetHashCodeFromException(exception, hashCode);
+
+            if (args == null)
+            {
+                logger.LogInformation(hashCode, exception, string.Empty);
+            }
+            else
+            {
+                logger.LogInformation(hashCode, exception, string.Empty, args);
+            }
+        }
 
         //------------------------------------------WARNING------------------------------------------//
 
@@ -159,13 +157,16 @@ namespace IT2media.Extensions.Logging.Abstractions
         /// <param name="args">An object array that contains zero or more objects to format.</param>
         public static void LogWarning(this ILogger logger, Exception exception, string message, params object[] args)
         {
-            if (message == null)
+            int hashCode = 0;
+            hashCode = GetHashCode(ref message, exception, hashCode);
+
+            if (args == null)
             {
-                logger.LogWarning(0, exception, string.Empty, args);
+                logger.LogWarning(hashCode, exception, message);
             }
             else
             {
-                logger.LogWarning(message.GetHashCodeUShort(), exception, message, args);
+                logger.LogWarning(hashCode, exception, message, args);
             }
         }
 
@@ -173,19 +174,22 @@ namespace IT2media.Extensions.Logging.Abstractions
         /// Formats and writes a warning log message.
         /// </summary>
         /// <param name="logger">The <see cref="ILogger"/> to write to.</param>
-        /// <param name="message">Format string of the log message.</param>
+        /// <param name="exception">The exception to log.</param>
         /// <param name="args">An object array that contains zero or more objects to format.</param>
-        //public static void LogWarning(this ILogger logger, string message, params object[] args)
-        //{
-        //    if (message == null)
-        //    {
-        //        logger.LogWarning(0, string.Empty, args);
-        //    }
-        //    else
-        //    {
-        //        logger.LogWarning(message.GetHashCodeUShort(), message, args);
-        //    }
-        //}
+        public static void LogWarning(this ILogger logger, Exception exception, params object[] args)
+        {
+            int hashCode = 0;
+            hashCode = GetHashCodeFromException(exception, hashCode);
+
+            if (args == null)
+            {
+                logger.LogWarning(hashCode, exception, string.Empty);
+            }
+            else
+            {
+                logger.LogWarning(hashCode, exception, string.Empty, args);
+            }
+        }
 
         //------------------------------------------ERROR------------------------------------------//
 
@@ -198,33 +202,39 @@ namespace IT2media.Extensions.Logging.Abstractions
         /// <param name="args">An object array that contains zero or more objects to format.</param>
         public static void LogError(this ILogger logger, Exception exception, string message, params object[] args)
         {
-            if (message == null)
+            int hashCode = 0;
+            hashCode = GetHashCode(ref message, exception, hashCode);
+
+            if (args == null)
             {
-                logger.LogError(0, exception, string.Empty, args);
+                logger.LogError(hashCode, exception, message);
             }
             else
             {
-                logger.LogError(message.GetHashCodeUShort(), exception, message, args);
+                logger.LogError(hashCode, exception, message, args);
             }
         }
 
         /// <summary>
-        /// Formats and writes an error log message.
+        /// Formats and writes a error log message.
         /// </summary>
         /// <param name="logger">The <see cref="ILogger"/> to write to.</param>
-        /// <param name="message">Format string of the log message.</param>
+        /// <param name="exception">The exception to log.</param>
         /// <param name="args">An object array that contains zero or more objects to format.</param>
-        //public static void LogError(this ILogger logger, string message, params object[] args)
-        //{
-        //    if (message == null)
-        //    {
-        //        logger.LogError(0, string.Empty, args);
-        //    }
-        //    else
-        //    {
-        //        logger.LogError(message.GetHashCodeUShort(), message, args);
-        //    }
-        //}
+        public static void LogError(this ILogger logger, Exception exception, params object[] args)
+        {
+            int hashCode = 0;
+            hashCode = GetHashCodeFromException(exception, hashCode);
+
+            if (args == null)
+            {
+                logger.LogError(hashCode, exception, string.Empty);
+            }
+            else
+            {
+                logger.LogError(hashCode, exception, string.Empty, args);
+            }
+        }
 
         //------------------------------------------CRITICAL------------------------------------------//
 
@@ -237,13 +247,16 @@ namespace IT2media.Extensions.Logging.Abstractions
         /// <param name="args">An object array that contains zero or more objects to format.</param>
         public static void LogCritical(this ILogger logger, Exception exception, string message, params object[] args)
         {
-            if (message == null)
+            int hashCode = 0;
+            hashCode = GetHashCode(ref message, exception, hashCode);
+
+            if (args == null)
             {
-                logger.LogCritical(0, exception, string.Empty, args);
+                logger.LogCritical(hashCode, exception, message);
             }
             else
             {
-                logger.LogCritical(message.GetHashCodeUShort(), exception, message, args);
+                logger.LogCritical(hashCode, exception, message, args);
             }
         }
 
@@ -251,18 +264,63 @@ namespace IT2media.Extensions.Logging.Abstractions
         /// Formats and writes a critical log message.
         /// </summary>
         /// <param name="logger">The <see cref="ILogger"/> to write to.</param>
-        /// <param name="message">Format string of the log message.</param>
+        /// <param name="exception">The exception to log.</param>
         /// <param name="args">An object array that contains zero or more objects to format.</param>
-        //public static void LogCritical(this ILogger logger, string message, params object[] args)
-        //{
-        //    if (message == null)
-        //    {
-        //        logger.LogCritical(0, string.Empty, args);
-        //    }
-        //    else
-        //    {
-        //        logger.LogCritical(message.GetHashCodeUShort(), message, args);
-        //    }
-        //}
+        public static void LogCritical(this ILogger logger, Exception exception, params object[] args)
+        {
+            int hashCode = 0;
+            hashCode = GetHashCodeFromException(exception, hashCode);
+
+            if (args == null)
+            {
+                logger.LogCritical(hashCode, exception, string.Empty);
+            }
+            else
+            {
+                logger.LogCritical(hashCode, exception, string.Empty, args);
+            }
+        }
+
+        #region GetHashCodes
+
+        /// <summary>
+        /// Returns an ushort hash based on the message, if message is null the exception's message is taken
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="exception"></param>
+        /// <param name="defaultHashCode"></param>
+        /// <returns></returns>
+        private static int GetHashCode(ref string message, Exception exception, int defaultHashCode)
+        {
+            if (message == null)
+            {
+                message = string.Empty;
+                defaultHashCode = GetHashCodeFromException(exception, defaultHashCode);
+            }
+            else
+            {
+                defaultHashCode = message.GetHashCodeUShort();
+            }
+
+            return defaultHashCode;
+        }
+
+        /// <summary>
+        /// Returns an ushort hash based on the exception's message
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <param name="defaultHashCode"></param>
+        /// <returns></returns>
+        private static int GetHashCodeFromException(Exception exception, int defaultHashCode)
+        {
+            if (exception != null && exception.Message != null) //currently this is not the case, but with different .NET Frameworks and Mono-Implementations we can't be sure, so let's do this safe
+            {
+                defaultHashCode = exception.Message.GetHashCodeUShort();
+            }
+
+            return defaultHashCode;
+        }
+
+        #endregion
     }
 }
